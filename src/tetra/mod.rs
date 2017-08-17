4dd3ac10f04f94a1f6dc141e835a5c608859cb0c
Tim Lovorn <tflovorn@austin.utexas.edu>
2017-08-17 08:00:38 -0500

    calculate orbital-resolved expectation values

--- src/tetra/mod.rs
+++ src/tetra/mod.rs
@@ -5,4 +5,4 @@ mod sum;
 
 pub use self::grid::{EnergyGrid, EvecGrid, grid_index, grid_k, EvecCache};
 pub use self::weight::{all_weights, band_weights};
-pub use self::sum::{band_expectation_value, expectation_value, band_number, total_number};
+pub use self::sum::{band_expectation_value, expectation_value, orbital_expectation_values, orbital_number, total_number};
--- src/tetra/sum.rs
+++ src/tetra/sum.rs
@@ -1,3 +1,5 @@
+use tetra::grid::EvecGrid;
+
 /// Calculate the contribution of one band to the expectation value of x.
 ///
 /// <x_n> = \sum_k x_{nk} w_{nk}
@@ -20,24 +22,46 @@ pub fn expectation_value(x_nk: &Vec<Vec<f64>>, weights: &Vec<Vec<f64>>) -> f64 {
         .sum()
 }
 
-/// Calculate the contribution of one band to the total electron density <n>.
+/// Calculate the orbital-resolved contributions to the expectation value of x.
 ///
-/// <n_n> = \sum_{k} w_{nk}
-pub fn band_number(band_weights: &Vec<f64>) -> f64 {
-    let x_k = vec![1.0; band_weights.len()];
+/// <x_i> = \sum_{nk} |U_{in}^k|^2 x_{nk} w_{nk}
+pub fn orbital_expectation_values<G: EvecGrid>(grid: &G, x_nk: &Vec<Vec<f64>>, weights: &Vec<Vec<f64>>) -> Vec<f64> {
+    let mut evs = vec![0.0; grid.bands()];
 
-    band_expectation_value(&x_k, &band_weights)
+    for (band_index, (x_ks, w_ks)) in x_nk.iter().zip(weights).enumerate() {
+        for (grid_index, (x, w)) in x_ks.iter().zip(w_ks).enumerate() {
+            for orbital_index in 0..grid.bands() {
+                let u = grid.evec(grid_index)[[orbital_index, band_index]];
+
+                evs[orbital_index] += u.norm_sqr() * x * w;
+            }
+        }
+    }
+
+    evs
 }
 
-/// Calculate the total electron density <n>.
-///
-/// <n> = \sum_{nk} w_{nk}
-pub fn total_number(weights: &Vec<Vec<f64>>) -> f64 {
+/// Operator elements n_{nk} = 1 for the number operator.
+fn num_x(weights: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let mut x_nk = Vec::new();
 
     for band_weights in weights {
         x_nk.push(vec![1.0; band_weights.len()]);
     }
 
-    expectation_value(&x_nk, &weights)
+    x_nk
+}
+
+/// Calculate the contributions of each orbital to the total electron density <n>.
+///
+/// <n_i> = \sum_{nk} |U_{in}^k|^2 w_{nk}
+pub fn orbital_number<G: EvecGrid>(grid: &G, weights: &Vec<Vec<f64>>) -> Vec<f64> {
+    orbital_expectation_values(grid, &num_x(&weights), &weights)
+}
+
+/// Calculate the total electron density <n>.
+///
+/// <n> = \sum_{nk} w_{nk}
+pub fn total_number(weights: &Vec<Vec<f64>>) -> f64 {
+    expectation_value(&num_x(&weights), &weights)
 }
